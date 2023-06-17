bd07d2b4d4515b14ecdd1a58cb7d4fa93464e706
johbry17 <73319490+johbry17@users.noreply.github.com>
2023-06-17 01:35:23 -0400

    barely pseudocode

---
+++ PyBank/main.py
@@ -0,0 +1,24 @@
+import csv
+
+def main():
+    # dataset of two columns, date and profit/losses
+    # first, open the file to read over
+    '''
+    with open(sys.argv[1]) as file:
+    reader = csv.DictReader(file)
+        for row in reader:
+            do stuff, like:
+    ''''
+        # find and declare variable column_length
+        # iterate over rows to column_length
+            # month variable++ that returns total number of months
+            # return change in profit/losses over entire time
+            # return average of changes in profit/losses over time
+            # greatest increase in profits (date and amount)
+            # greatest decrease in profits (date and amount)
+
+    # think about how to do below as separate functions, to reuse later    
+    # print all to terminal
+        #print function
+    # export results to text file with results
+        #open a new file, write results, save to analysis folder
