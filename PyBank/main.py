f01e503f7847dfbc5cf341ca0e7ef70c889233b9
johbry17 <73319490+johbry17@users.noreply.github.com>
2023-06-21 14:04:02 -0400

    added draft of output print function

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,11 +5,17 @@ def main():
     # first, open the file to read over
     '''
     with open(sys.argv[1]) as file:
-    reader = csv.DictReader(file)
-        for row in reader:
-            do stuff, like:
-    ''''
-        # find and declare variable column_length
+        reader = csv.DictReader(file, filednames["header1", "header2"])
+            {"header1": value, "header2": value}
+            for row in reader:
+                do stuff, like:
+    '''
+        # with open("*.csv") as file:
+            #store headers?
+            # for line in file:
+                # date, profit_or_loss = line.rstrip().split(",")
+
+        # find and declare variable column_length - maybe unnecessary?
         # iterate over rows to column_length
             # month variable++ that returns total number of months
             # return change in profit/losses over entire time
@@ -17,8 +23,23 @@ def main():
             # greatest increase in profits (date and amount)
             # greatest decrease in profits (date and amount)
 
-    # think about how to do below as separate functions, to reuse later    
     # print all to terminal
-        #print function
+    print_output()
     # export results to text file with results
         #open a new file, write results, save to analysis folder
+
+
+# prints everything to terminal
+#TODO
+# add add input for months, total_change, avg_change, increase, decrease
+def print_output():
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months:")
+    print("Total:")
+    print("Average Change:")
+    print("Greatest Increase in Profits:")
+    print("Greatest Decrease in Profits:")
+
+
+main()
