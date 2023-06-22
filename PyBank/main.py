d783515659b5879f0be3e7f781ca992a70f319c5
johbry17 <73319490+johbry17@users.noreply.github.com>
2023-06-22 05:39:09 -0400

    pybank completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,21 @@
 import csv
+import sys
+
 
 def main():
-
     # declare an empty list [] to store the csv
     list = []
 
     # first, open the file to read over
-    with open('./Resources/budget_data.csv') as file:
+    with open("./Resources/budget_data.csv") as file:
         reader = csv.DictReader(file)
-            # TODO Successfully store header row - Why?
-            # this append loop will successfully re-write the headers anyway
-            # maybe that's all they mean in the instructions?
+        # TODO Successfully store header row - Why?
+        # this append loop will successfully re-write the headers anyway
+        # maybe that's all they mean in the instructions?
         header = reader.fieldnames
         # store csv file in list
         for row in reader:
-                list.append(row)
+            list.append(row)
 
     # call function to iterate over list and extract results
     do_stuff(list)
@@ -22,7 +23,6 @@ def main():
 
 # finds requested data
 def do_stuff(info):
-    
     # declare variables
     month_count = 0
     net_profit = 0
@@ -30,41 +30,39 @@ def do_stuff(info):
     increase = 0
     decrease = 0
 
-    
     for dict in info:
-         
-         # grab values for current row's cells
-         monthly_profit = int(list(dict.values())[1])
-         date = str(list(dict.values())[0])
+        # grab values for current row's cells
+        monthly_profit = int(list(dict.values())[1])
+        date = str(list(dict.values())[0])
 
-         # adds current months profit to net_profit
-         net_profit = net_profit + monthly_profit
-         
-         # sets some initial variables
-         if month_count == 0:
+        # adds current months profit to net_profit
+        net_profit = net_profit + monthly_profit
+
+        # sets some initial variables
+        if month_count == 0:
             last_month = monthly_profit
             total_change = 0
             month_count += 1
         # does the work
-         else:
+        else:
             # computes change in profit between the current_month and the last_month
             current_month = monthly_profit
             change = current_month - last_month
             last_month = monthly_profit
             # updates total_change and month_count
             total_change = total_change + change
-            month_count +=1
+            month_count += 1
 
-         # compares change with increase or decrease, updates if a more extreme value is found
-         if change > increase:
+        # compares change with increase or decrease, updates if a more extreme value is found
+        if change > increase:
             increase = change
             increase_date = date
-         if change < decrease:
-             decrease = change
-             decrease_date = date
+        if change < decrease:
+            decrease = change
+            decrease_date = date
 
     # calculates average of changes in profit/losses over time
-    average = total_change / (month_count-1)
+    average = total_change / (month_count - 1)
 
     # call function to print results to terminal
     months = month_count
@@ -74,9 +72,19 @@ def do_stuff(info):
     decrease = f"{decrease_date} (${decrease})"
     print_output(months, total_change, avg_change, increase, decrease)
 
-    # TODO
     # export results to text file with results
-        #open a new file, write results, save to analysis folder
+    # Save the original stdout
+    original_stdout = sys.stdout
+
+    # Open a file for writing
+    with open("./analysis/output.txt", "w") as file:
+        # Redirect the stdout to the file
+        sys.stdout = file
+        print_output(months, total_change, avg_change, increase, decrease)
+
+    # Restore the original stdout
+    sys.stdout = original_stdout
+
 
 # prints everything to terminal
 def print_output(months, total_change, avg_change, increase, decrease):
